1cd4c70017b98850bdf39806c1903a5a9f476cfa
Stephen Booth <s.booth@ed.ac.uk>
2015-12-10 16:37:49 +0000

    track cvs changes

--- src/uk/ac/ed/epcc/safe/accounting/parsers/AlpsLogParser.java
+++ src/uk/ac/ed/epcc/safe/accounting/parsers/AlpsLogParser.java
@@ -13,6 +13,7 @@ import uk.ac.ed.epcc.safe.accounting.update.AccountingParseException;
 import uk.ac.ed.epcc.safe.accounting.update.AutoTable;
 import uk.ac.ed.epcc.safe.accounting.update.IncrementalPropertyContainerParser;
 import uk.ac.ed.epcc.webapp.AppContext;
+import uk.ac.ed.epcc.webapp.logging.Logger;
 
 /** Parser for alps syslog information
  * 
@@ -44,6 +45,7 @@ public class AlpsLogParser extends AbstractPropertyContainerParser implements In
 	@AutoTable(target=Date.class)
 	public static final PropertyTag<Date> APSYS_END_TIMESTAMP = new PropertyTag<Date>(alps_reg, "apsysEndTime", Date.class, "Timestamp of finishing apsys record");
 	
+	Logger log;
 	
 	@Override
 	public boolean parse(PropertyMap map, String record) throws AccountingParseException {
@@ -65,7 +67,89 @@ public class AlpsLogParser extends AbstractPropertyContainerParser implements In
 
 	@Override
 	public boolean isComplete(UsageRecord record) {
-		// TODO Auto-generated method stub
+		// We need the set of properties from both record types, aprun and apsys
+				
+		// BATCH_ID
+		String batch_id = record.getProperty(BATCH_ID, null);
+		if(batch_id == null) {
+			log.debug("No batch_id");
+			return false;
+		}
+		log.debug("batch_id is " + batch_id);
+		
+		// ALPS_ID
+		Integer apid = record.getProperty(ALPS_ID, null);
+		if(apid == null) {
+			log.debug("No apid");
+			return false;
+		}
+		log.debug("apid is " + apid);
+		
+		// USER
+		Integer user = record.getProperty(USER, null);
+		if(user == null) {
+			log.debug("No user");
+			return false;
+		}
+		log.debug("user is " + user);
+		
+		// CMD_LINE
+		String cmd_line = record.getProperty(CMD_LINE, null);
+		if(cmd_line == null) {
+			log.debug("No cmd_line");
+			return false;
+		}
+		log.debug("cmd_line is " + cmd_line);
+		
+		// CWD
+		String cwd = record.getProperty(CWD, null);
+		if(cwd == null) {
+			log.debug("No cwd");
+			return false;
+		}
+		log.debug("cwd is " + cwd);
+		
+		// NODE_LIST
+		String node_list = record.getProperty(NODE_LIST, null);
+		if(node_list == null) {
+			log.debug("No node_list");
+			return false;
+		}
+		log.debug("node_list is " + node_list);
+		
+		// NUM_NODES
+		Integer num_nodes = record.getProperty(NUM_NODES, null);
+		if(num_nodes == null) {
+			log.debug("No num_nodes");
+			return false;
+		}
+		log.debug("num_nodes is " + num_nodes);
+		
+		// HOSTNAME
+		String hostname = record.getProperty(HOSTNAME, null);
+		if(hostname == null) {
+			log.debug("No hostname");
+			return false;
+		}
+		log.debug("hostname is " + hostname);
+
+		// APRUN_START_TIMESTAMP
+		Date aprunStartTime = record.getProperty(APRUN_START_TIMESTAMP, null);
+		if(aprunStartTime == null) {
+			log.debug("No aprunStartTime");
+			return false;
+		}
+		log.debug("aprunStartTime is " + aprunStartTime);
+		
+		// APSYS_END_TIMESTAMP
+		Date apsysEndTime = record.getProperty(APSYS_END_TIMESTAMP, null);
+		if(apsysEndTime == null) {
+			log.debug("No apsysEndTime");
+			return false;
+		}
+		log.debug("apsysEndTime is " + apsysEndTime);
+		
+		log.debug("Record is complete");
 		return true;
 	}
 
