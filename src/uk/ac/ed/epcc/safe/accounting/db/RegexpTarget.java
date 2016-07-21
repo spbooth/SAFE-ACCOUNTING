9379bfe8b57bd0d1da4f0a994ab846c60a2d174f
Michael Bareford <bareford@mbfiltered.com>
2016-07-21 10:38:04 +0100

    Added getIdentifier and getName methods.

--- src/uk/ac/ed/epcc/safe/accounting/db/RegexpTarget.java
+++ src/uk/ac/ed/epcc/safe/accounting/db/RegexpTarget.java
@@ -6,6 +6,11 @@ import uk.ac.ed.epcc.webapp.model.data.Repository.Record;
 
 public class RegexpTarget extends DataObjectPropertyContainer {
 
+	@Override
+	public String getIdentifier(int max_length) {
+		return getName();
+	}
+
 	public RegexpTarget(RegexpTargetFactory fac, Record r) {
 		super(fac, r);
 	}
@@ -14,4 +19,7 @@ public class RegexpTarget extends DataObjectPropertyContainer {
 		return Pattern.compile(record.getStringProperty(RegexpTargetFactory.REGEX_FIELD));
 	}
 
+	public String getName(){
+		return record.getStringProperty(RegexpTargetFactory.NAME_FIELD);
+	}
 }
