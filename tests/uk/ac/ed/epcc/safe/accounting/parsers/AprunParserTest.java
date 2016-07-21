83f56821fe7759a60c9edae7fc10bb6550df0f44
Michael Bareford <bareford@mbfiltered.com>
2016-07-21 10:46:20 +0100

    Rearranged test data.

--- tests/uk/ac/ed/epcc/safe/accounting/parsers/AprunParserTest.java
+++ tests/uk/ac/ed/epcc/safe/accounting/parsers/AprunParserTest.java
@@ -30,11 +30,13 @@ public class AprunParserTest extends AbstractRecordTestCase {
 	 */
 	static {
 		String good[] = new String[] {
-		"/opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -n 1024 ./imagenew 1024 1",
-		"/opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -n 1000 ./imagenew 1024 1",
-		"/opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -n 1 /work/e280/e280/sv375/Solvaware_v1.3/Solvaware_Trunk/HydrationSiteAnalysis/Utilities/WriteDensityProfile /fs2/e280/e280/sv375/carbonic_anhydrase_2/2NNG/Equilibration/2NNG_system.pdb /fs2/e280/e280/sv375/carbonic_anhydrase_2/2NNG/Equilibration/2NNG_system.psf /fs2/e280/e280/sv375/carbonic_anhydrase_2/2NNG/Dynamics/2NNG_system_dynamics.dcd 0 9000 1 1.2 59 59 59 108 480",
-		"/opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -N 1 -n 1 -S 1 /work/n02/n02/pappas/src/oasis/crayxe6_cce-20160403/bin/oasis3.MPI1.x : -N 24 -d 1 -n 144 -S 12 -ss /work/n02/n02/pappas/um/xgspt/bin/ga30_kpp.exe-20160403 : -N 1 -n 1 -d 24 /work/n02/n02/bdong/um/xmnub/dataw/KPP_ocean",
-		"/opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -ss -n 1920 -N 24 -d 1 -S 12 -j 1 -e OMP_NUM_THREADS=1 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe : -ss -n 24 -N 12 -d 2 -S 6 -j 1 -e OMP_NUM_THREADS=2 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe"
+		"apid=21699839 /opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -n 1024 ./imagenew 1024 1",
+		"apid=21699856 /opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -n 1000 ./imagenew 1024 1",
+		"apid=21699984 /opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -n 1 /work/e280/e280/sv375/Solvaware_v1.3/Solvaware_Trunk/HydrationSiteAnalysis/Utilities/WriteDensityProfile /fs2/e280/e280/sv375/carbonic_anhydrase_2/2NNG/Equilibration/2NNG_system.pdb /fs2/e280/e280/sv375/carbonic_anhydrase_2/2NNG/Equilibration/2NNG_system.psf /fs2/e280/e280/sv375/carbonic_anhydrase_2/2NNG/Dynamics/2NNG_system_dynamics.dcd 0 9000 1 1.2 59 59 59 108 480",
+		"apid=21701310 /opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -N 1 -n 1 -S 1 /work/n02/n02/pappas/src/oasis/crayxe6_cce-20160403/bin/oasis3.MPI1.x : -N 24 -d 1 -n 144 -S 12 -ss /work/n02/n02/pappas/um/xgspt/bin/ga30_kpp.exe-20160403 : -N 1 -n 1 -d 24 /work/n02/n02/bdong/um/xmnub/dataw/KPP_ocean",
+		"apid=21705594 /opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -ss -n 1920 -N 24 -d 1 -S 12 -j 1 -e OMP_NUM_THREADS=1 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe : -ss -n 24 -N 12 -d 2 -S 6 -j 1 -e OMP_NUM_THREADS=2 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe",
+		"apid=21705595 /opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun --ss -n 1920 -N 24 -d 1 -S 12 -j 1 -e OMP_NUM_THREADS=1 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe",
+		"apid=21705596 /opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -ss -n 24 -N 12 -d 2 -S 6 -j1 -e OMP_NUM_THREADS=2 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe"
 		};
 		
 		for( String s : good){
@@ -45,10 +47,9 @@ public class AprunParserTest extends AbstractRecordTestCase {
 		
 		//TODO: setup negative test cases
 		String bad[] = new String[] {
-		"/opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun --ss -n 1920 -N 24 -d 1 -S 12 -j 1 -e OMP_NUM_THREADS=1 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe",
-		"/opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -ss -n 24 -N 12 -d 2 -S 6 -j1 -e OMP_NUM_THREADS=2 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe",
-		"/opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -ss -n 24 -N 12 -d 2 -S 6 -j1 env OMP_NUM_THREADS=2 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe"
+		"apid=21705597 /opt/cray/alps/5.2.3-2.0502.9295.14.14.ari/bin/aprun -ss -n 24 -N 12 -d 2 -S 6 -j1 env OMP_NUM_THREADS=2 /work/n02/n02/wmcginty/um/xmrfd/bin/xmrfd.exe"
 		};
+		
 		/*
 		for( String s : bad){
 			Exception x = new AccountingParseException();
