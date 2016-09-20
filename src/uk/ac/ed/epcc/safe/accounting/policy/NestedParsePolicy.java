561204f04d67fd540cdb86f5b8c4311db9da63d0
Michael Bareford <michael.bareford@epcc.ed.ac.uk>
2016-09-20 13:15:40 +0100

    Removed comments concerning RURLog.

--- src/uk/ac/ed/epcc/safe/accounting/policy/NestedParsePolicy.java
+++ src/uk/ac/ed/epcc/safe/accounting/policy/NestedParsePolicy.java
@@ -72,14 +72,6 @@ public class NestedParsePolicy extends BaseUsageRecordPolicy {
 		
 		return prev;
 		
-		//TODO: see below
-		/**
-		 * Process the init parameters concerned with rur logs.
-		 * 
-		 * nested_parse.rur_table.ALPSLog=RURLog
-		 * nested_parse.rur_logs_loc.ALPSLog=./logs
-		 */
-		
 	}
 
 	
@@ -95,8 +87,6 @@ public class NestedParsePolicy extends BaseUsageRecordPolicy {
 				    update = link_prop_name + "=" + rec.getProperty(link_prop).toString() + " " + update;
 				}
 				
-				
-				
 				PropertyMap meta_data = new PropertyMap();      
 			    meta_data.setAll(props);
 			    if (parent_tag != null) {
@@ -110,13 +100,6 @@ public class NestedParsePolicy extends BaseUsageRecordPolicy {
 			}
 			
 		}
-		
-		//TODO: see below
-		/**
-		 * Add code that will search suitable rurlog files for entries that match the alps id stored by rec.
-		 * These entries will be concatenated and passed to the RurLogParser via a suitably constructed
-		 * AccountingUpdater object.
-		 */
 		
 	}
 
