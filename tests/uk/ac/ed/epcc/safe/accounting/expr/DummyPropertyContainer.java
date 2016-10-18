9991fa4be97db885d700300bf45ecbeafbbabd91
Michael Bareford <michael.bareford@epcc.ed.ac.uk>
2016-10-18 10:07:52 +0100

    Removed blank line.

--- tests/uk/ac/ed/epcc/safe/accounting/expr/DummyPropertyContainer.java
+++ tests/uk/ac/ed/epcc/safe/accounting/expr/DummyPropertyContainer.java
@@ -3,7 +3,6 @@ package uk.ac.ed.epcc.safe.accounting.expr;
 import uk.ac.ed.epcc.safe.accounting.db.DataObjectPropertyContainer;
 import uk.ac.ed.epcc.webapp.model.data.Repository;
 
-
 public class DummyPropertyContainer extends DataObjectPropertyContainer{
 
 	public DummyPropertyContainer(DummyPropertyFactory fac, Repository.Record r) {
