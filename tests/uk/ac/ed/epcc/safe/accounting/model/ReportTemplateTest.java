54eabd3c3af471d090950d0a9fc7c7366744b68c
Stephen Booth <s.booth@epcc.ed.ac.uk>
2018-02-08 23:03:39 +0000

    tests

--- tests/uk/ac/ed/epcc/safe/accounting/model/ReportTemplateTest.java
+++ tests/uk/ac/ed/epcc/safe/accounting/model/ReportTemplateTest.java
@@ -6,11 +6,24 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+
 import org.junit.Test;
 
 import uk.ac.ed.epcc.safe.accounting.reports.ReportBuilder;
 import uk.ac.ed.epcc.webapp.WebappTestBase;
+import uk.ac.ed.epcc.webapp.content.Button;
+import uk.ac.ed.epcc.webapp.content.HtmlBuilder;
+import uk.ac.ed.epcc.webapp.content.Table;
+import uk.ac.ed.epcc.webapp.content.TableXMLFormatter;
+import uk.ac.ed.epcc.webapp.forms.registry.FormOperations;
+import uk.ac.ed.epcc.webapp.forms.result.ChainedTransitionResult;
+import uk.ac.ed.epcc.webapp.forms.transition.TransitionFactory;
 import uk.ac.ed.epcc.webapp.junit4.DataBaseFixtures;
+import uk.ac.ed.epcc.webapp.model.data.Exceptions.DataFault;
+import uk.ac.ed.epcc.webapp.servlet.TransitionServlet;
 
 
 public class ReportTemplateTest extends WebappTestBase{
@@ -54,6 +67,27 @@ public class ReportTemplateTest extends WebappTestBase{
 		
 		assertNotNull(reportTemplateFactory.findByFileName("UserJobs.xml"));
 		reportBuilder = new ReportBuilder(ctx,"UserJobs","report.xsd");	
+		
+	}
+	
+	@Test
+	@DataBaseFixtures({"ReportTemplateData.xml"})
+	public void testIndexTable() throws DataFault, TransformerConfigurationException, TransformerFactoryConfigurationError, TransformerException {
+		ReportTemplateFactory reportTemplateFactory = ctx.makeObject(ReportTemplateFactory.class,"ReportTemplate");
+		Table<String,ReportTemplate> t= reportTemplateFactory.getIndexTable();
+		HtmlBuilder builder = new HtmlBuilder();
+		
+//		TransitionFactory provider = TransitionServlet.getProviderFromName(ctx,"Reports:ReportTemplate");
+//		assertNotNull(provider);
+//		for(ReportTemplate r : t.getRows()){
+//			if( provider.allowTransition(ctx, r, FormOperations.Edit)){
+//				t.put("Edit", r, new Button(ctx, "Edit",new ChainedTransitionResult(provider,r,FormOperations.Edit)));
+//			}
+//		}
+		
+	  	TableXMLFormatter<String,ReportTemplate> fmt = new TableXMLFormatter<String,ReportTemplate>(builder, null);
+	  	fmt.add(t);
+	  	checkContent("/normalize.xsl", "report_index.xml", builder.toString());
 		
 	}
 }
