e94c043cd7e8471db346bfe9dbe8efcbf22d265b
Stephen Booth <s.booth@epcc.ed.ac.uk>
2019-04-26 12:28:20 +0100

    fix default test path locations

--- src/test/java/uk/ac/ed/epcc/safe/accounting/parsers/AbstractRecordTestCase.java
+++ src/test/java/uk/ac/ed/epcc/safe/accounting/parsers/AbstractRecordTestCase.java
@@ -466,6 +466,6 @@ public abstract class AbstractRecordTestCase<R> extends
 	public File getFixtureDir(){
 		return new File(
 				ctx.getInitParameter("test.fixture.dir",
-				"src/test/java/uk/ac/ed/epcc/safe/accounting/parsers/ogfur-records"));
+				"src/test/resources/uk/ac/ed/epcc/safe/accounting/parsers/ogfur-records"));
 	}
 }
